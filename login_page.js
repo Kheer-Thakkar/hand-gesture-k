a4cbb39e9e49686e6ee3b6e7516e19b72b91c5f8
Kheer-Thakkar <77600784+Kheer-Thakkar@users.noreply.github.com>
2021-05-16 01:40:00 +0530

    Add files via upload

---
+++ login_page.js
@@ -0,0 +1,8 @@
+function adduser(){
+    user_name=document.getElementById("user_name").value;
+   password=document.getElementById("password").value;
+   localStorage.setItem("user_name",user_name);
+   localStorage.setItem("password",password);
+   window.location="index.html";
+    
+  }
